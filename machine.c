954f2a9ab7d62d04ba33ffeca2bd3a17ba5a3bb4
Eva-M-Lopez <eva.m.lopez2004@gmail.com>
2024-09-25 17:12:56 -0400

    Update machine.c

--- machine.c
+++ machine.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    unsigned int PC;
+    unsigned int gp;
+    unsigned int fp;
+    unsigned int sp;
+}VMReg;
+
+VMReg reg;
+
+
 //a size for the memory(2^16 words = 32k words)
 #define MEMORY_SIZE_IN_WORDS 32768
 
@@ -28,6 +38,7 @@ unsigned char* read_file(const char *fileName, long *fileSize) {
         return NULL;
     }
 
+    
     size_t bytes_read = fread(buffer, 1, *fileSize, file);
     //memory allocation fails
     if(bytes_read != *fileSize) {
@@ -36,12 +47,35 @@ unsigned char* read_file(const char *fileName, long *fileSize) {
         return NULL;
     }
 
+    //read the header information
+    unsigned int text_length = *(unsigned int*)(buffer);
+    //read first 4 bytes
+    unsigned int data_start_address = *(unsigned int*)(buffer + 4);
+    //read from 4-7
+    unsigned int stack_bottom_address = *(unsigned int*)(buffer + 8);
+    //read from 8-11
+
+    //initialize the registers
+    reg.PC = 0;
+    reg.gp = data_start_address;
+    reg.fp =stack_bottom_address;
+    reg.sp = text_start_address;
+
+    //get the text section of the bof file
+    for(int i =0;I<text_length;i++){
+        
+    }
+
+
     fclose(file);
     //returns the contents of the file
     return buffer;
 
 }
 
+
+
+    
 
 
 
