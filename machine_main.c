c4af18b980e52c7679f7eef239adaf41d37340fc
Eva-M-Lopez <eva.m.lopez2004@gmail.com>
2024-09-26 13:53:37 -0400

    Update machine_main.c

--- machine_main.c
+++ machine_main.c
@@ -19,6 +19,8 @@ int main(int argc, char *argv[]) {
 
     printf("flag is %d\n", pFlag);
 
+    //load information
+    read_file(const char *fileName, long *fileSize) {
 
     //if pFlag is true print out the instruction. (Result would ble vm_test.lst for example)
     if(pFlag == 1) {
